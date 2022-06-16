72b4a8f33fb1bf2833f6ce52fec2250017bb6e96
Manav Nanwani <manav.nanwani@gmail.com>
2022-06-16 21:54:06 +0530

    Time: 44 ms (69.90%), Space: 24.7 MB (42.11%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,34 +1,24 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        string ans = "";
-        string check;
-        int aLen = 0,cLen = 0;
-        int l,r;
+        int l=0, r=0, temp=0;
+        string ans;
         for(int i=0; i<s.length(); i++){
-            l=i, r=i;
-            while(l>=0 && r<s.length() && s[l] == s[r]){
-                // cLen = r - l + 1; 
-                // check = s.substr(l,cLen);
-                if(r-l+1 > aLen){
-                    aLen = r - l + 1;
-                    ans = s.substr(l, aLen);
-                    // aLen = cLen;                    
+            l=i; r=i;
+            while(l>=0 && r<s.length() && s[l]==s[r]){
+                if(r-l+1 > temp){
+                    temp = r-l+1;
+                    ans = s.substr(l, temp);
                 }
-                l--;
-                r++;
+                l--; r++;
             }
-
-            l=i, r=i+1;
-            while(l>=0 && r<s.length() && s[l] == s[r]){
-                // cLen = r - l + 1; 
-                // check = s.substr(l,r-l+1);
-                if(r-l+1 > aLen){
-                    aLen = r - l + 1;
-                    ans = s.substr(l, aLen);
+            l=i; r=i+1;
+            while(l>=0 && r<s.length() && s[l]==s[r]){
+                if(r-l+1 > temp){
+                    temp = r-l+1;
+                    ans = s.substr(l, temp);
                 }
-                l--;
-                r++;
+                l--; r++;
             }
         }
         return ans;
