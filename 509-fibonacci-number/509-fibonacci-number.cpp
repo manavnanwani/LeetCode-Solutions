dd65335f369b9bb286977880c3fb70dd48a30230
Manav Nanwani <manav.nanwani@gmail.com>
2022-07-06 22:01:27 +0530

    Time: 0 ms (100.00%), Space: 5.7 MB (96.23%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,10 +1,17 @@
 class Solution {
 public:
+    int memo[31] = {0};
     int fib(int n) {
-        if(n==0)
-            return 0;
-        if(n==1)
-            return 1;
-        return fib(n-1) + fib(n-2);
+        // if(n==0)
+        //     return 0;
+        // if(n==1)
+        //     return 1;
+        // return fib(n-1) + fib(n-2);
+        
+        if(n <= 1)
+            return n;
+        if(memo[n])
+            return memo[n];
+        return memo[n] = fib(n-1) + fib(n-2);
     }
 };
